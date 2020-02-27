f92e31de17a7a4615b5a75707589ff132d288f07
White <WhiteM@dnb.com>
2020-02-27 13:54:20 -0600

    add user profile page

--- app/routes.py
+++ app/routes.py
@@ -54,13 +54,17 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Regsiter', form=form)
 
-
-
-
-
-
-
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+            {'author':user, 'body': 'Test post #  1'},
+            {'author':user, 'body': 'Test post #  2'}
+            ]
+    return render_template('user.html', user=user, posts=posts)
